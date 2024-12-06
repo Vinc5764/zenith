80e606826e554672ce16e057a942beb76394256c
simonn70 <simonadjei70@gmail.com>
2024-12-06 17:20:55 +0000

    update stuff

--- app/api/partner/route.ts
+++ app/api/partner/route.ts
@@ -76,22 +76,22 @@ export const POST = async (req: any) => {
   }
 };
 
-export const GET = async (req: any, { params }: any) => {
-  try {
-    // Connect to MongoDB database
-    await connectToDB();
+// export const GET = async (req: any, { params }: any) => {
+//   try {
+//     // Connect to MongoDB database
+//     await connectToDB();
 
-    // Retrieve all users from the database
-    const users = await User.find(); // Use appropriate query if needed
+//     // Retrieve all users from the database
+//     const users = await User.find(); // Use appropriate query if needed
 
-    // Respond with list of users
-    return NextResponse.json(users);
-  } catch (error) {
-    // Handle errors
-    console.error("Error retrieving users:", error);
-    return NextResponse.json(
-      { message: "Failed to retrieve users" },
-      { status: 500 }
-    );
-  }
-};
+//     // Respond with list of users
+//     return NextResponse.json(users);
+//   } catch (error) {
+//     // Handle errors
+//     console.error("Error retrieving users:", error);
+//     return NextResponse.json(
+//       { message: "Failed to retrieve users" },
+//       { status: 500 }
+//     );
+//   }
+// };
