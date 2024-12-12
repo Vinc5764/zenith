12dff6d0825d62a3c674edaef21d0ab32a6822a0
simonn70 <simonadjei70@gmail.com>
2024-12-12 18:10:33 +0000

    update stuff

--- app/api/partner/partner-details/route.ts
+++ app/api/partner/partner-details/route.ts
@@ -19,8 +19,8 @@ export const GET = async (req: any, { params }: any) => {
 
     // const { mymembers } = params;
 
-    const { searchParams } = new URL(req.url);
-    const partner = searchParams.get("partner");
+
+  
     // Retrieve all users from the database
     const users = await User.find({role:"member"}); // Use appropriate query if needed
 
