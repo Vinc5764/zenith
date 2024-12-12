6186b703179480fc2a8edb7ca43ba8b13e439cde
simonn70 <simonadjei70@gmail.com>
2024-12-12 23:13:55 +0000

    update stuff

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,16 +5,25 @@ import { NextResponse } from "next/server";
 import User from "../model/user.model";
 
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export const GET = async (req: any, { params }: any) => {
   try {
     // Connect to MongoDB database
     await connectToDB();
 
-    // Retrieve all users from the database with `.exec()`
-    const users = await User.find().exec(); // Explicitly execute the query
+    // Retrieve all users from the database
+    const users = await User.find().exec();
 
-    // Respond with the list of users
-    return NextResponse.json(users);
+    // Prevent caching
+    const response = NextResponse.json(users);
+    response.headers.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Surrogate-Control", "no-store");
+
+    return response;
   } catch (error) {
     // Handle errors
     console.error("Error retrieving users:", error);
